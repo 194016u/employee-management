ed5891c6aff6fc785cdc92cbec765cdadbf058cc
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-08 00:56:08 +0530

    create product

---
+++ src/main/java/com/example/employeemanagement/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.example.employeemanagement.controller;
+
+import com.example.employeemanagement.dto.ProductDTO;
+import com.example.employeemanagement.dto.ResponseDTO;
+import com.example.employeemanagement.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ResponseDTO responseDTO;
+
+    @GetMapping("/getAllProduct")
+    public ResponseEntity getAllProducts(){
+        return productService.getAllProduct();
+    }
+
+    @PostMapping(value = "/saveProduct")
+    public ResponseEntity saveProduct(@RequestBody ProductDTO productDTO){
+        return new ResponseEntity(productService.saveProduct(productDTO), HttpStatus.ACCEPTED);
+    }
+
+
+}
---
+++ src/main/java/com/example/employeemanagement/service/ProductService.java
@@ -0,0 +1,59 @@
+package com.example.employeemanagement.service;
+
+import com.example.employeemanagement.dto.ProductDTO;
+import com.example.employeemanagement.dto.ResponseDTO;
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.entity.Product;
+import com.example.employeemanagement.repo.ProductRepo;
+import com.example.employeemanagement.util.VarList;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductService {
+    @Autowired
+    private ResponseDTO responseDTO;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ResponseEntity getAllProduct(){
+        List<Product> productList=productRepo.findAll();
+        responseDTO.setCode(VarList.RSP_SUCCESS);
+        responseDTO.setMessage("Success");
+        responseDTO.setContent(productList);
+        return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+    }
+    public ResponseEntity saveProduct(ProductDTO productDTO){
+        try{
+            if (productRepo.existsById(productDTO.getProdID())) {
+                responseDTO.setCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMessage("Product Registered");
+                responseDTO.setContent(productDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            } else {
+                productRepo.save(modelMapper.map(productDTO, Product.class));
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(productDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }
+        }catch (Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
