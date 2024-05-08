a3aeb512c3e8e3abc62049fa0c448c331f4f0ed4
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-08 22:17:20 +0530

    add to chart post request

--- src/main/java/com/example/employeemanagement/controller/CartItemController.java
+++ src/main/java/com/example/employeemanagement/controller/CartItemController.java
@@ -1,10 +1,12 @@
 package com.example.employeemanagement.controller;
 
+import com.example.employeemanagement.dto.CartItemDTO;
 import com.example.employeemanagement.dto.ResponseDTO;
 import com.example.employeemanagement.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/cart")
@@ -14,5 +16,23 @@ public class CartItemController {
 
     @Autowired
     private ResponseDTO responseDTO;
+
+    @PostMapping("/addToCart")
+    public ResponseEntity<ResponseDTO> addToCart(@RequestBody CartItemDTO cartItemDTO) {
+        ResponseDTO responseDTO = cartItemService.addToCart(cartItemDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.ACCEPTED);
+    }
+
+//    @GetMapping("/getCartItems/{empID}")
+//    public ResponseEntity<ResponseDTO> getCartItems(@PathVariable int empID) {
+//        ResponseDTO responseDTO = cartItemService.getCartItems(empID);
+//        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/removeFromCart/{empID}/{prodID}")
+//    public ResponseEntity<ResponseDTO> removeFromCart(@PathVariable int empID, @PathVariable int prodID) {
+//        ResponseDTO responseDTO = cartItemService.removeFromCart(empID, prodID);
+//        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+//    }
 
 }
--- src/main/java/com/example/employeemanagement/service/CartItemService.java
+++ src/main/java/com/example/employeemanagement/service/CartItemService.java
@@ -1,4 +1,57 @@
 package com.example.employeemanagement.service;
 
+import com.example.employeemanagement.dto.CartItemDTO;
+import com.example.employeemanagement.dto.ResponseDTO;
+import com.example.employeemanagement.entity.CartItem;
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.entity.Product;
+import com.example.employeemanagement.repo.CartItemRepo;
+import com.example.employeemanagement.repo.EmployeeRepo;
+import com.example.employeemanagement.repo.ProductRepo;
+import com.example.employeemanagement.util.VarList;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartItemService {
+
+    @Autowired
+    private CartItemRepo cartItemRepo;
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ResponseDTO addToCart(CartItemDTO cartItemDTO) {
+        ResponseDTO responseDTO = new ResponseDTO();
+        try {
+            Employee employee = employeeRepo.findById(cartItemDTO.getEmpId()).orElse(null);
+            Product product = productRepo.findById(cartItemDTO.getProdId()).orElse(null);
+            if (employee != null && product != null) {
+                CartItem cartItem = new CartItem();
+                cartItem.setEmployee(employee);
+                cartItem.setProduct(product);
+                cartItem.setQuantity(cartItemDTO.getQuantity());
+                cartItemRepo.save(cartItem);
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Item added to cart successfully");
+                responseDTO.setContent(cartItemDTO);
+            } else {
+                responseDTO.setCode(VarList.RSP_ERROR);
+                responseDTO.setMessage("Employee or Product not found");
+            }
+        } catch (Exception ex) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+        }
+        return responseDTO;
+    }
+
+
 }
