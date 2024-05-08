c8865ee5a5bc8d35427a38df3f113e1396ab964d
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-08 21:20:54 +0530

    add cart item DTO

---
+++ src/main/java/com/example/employeemanagement/dto/CartItemDTO.java
@@ -0,0 +1,14 @@
+package com.example.employeemanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDTO {
+    private int empId;
+    private int prodId;
+    private int quantity;
+}
