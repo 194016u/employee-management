3cf56c24dd8cded2700af9a63874f09c64a823a1
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-05-10 16:05:17 +0530

    admin eDTO

--- src/main/java/com/example/employeemanagement/dto/AdminDTO.java
+++ src/main/java/com/example/employeemanagement/dto/AdminDTO.java
@@ -1,4 +1,16 @@
 package com.example.employeemanagement.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AdminDTO {
+
+    private int adminId;
+    private String userName;
+    private String password;
+
 }
